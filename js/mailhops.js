df433e6038aebd32228270cc27112ad5ea84d6ed
Andrew Van Tassel <avantassel@gmail.com>
2024-07-24 15:33:05 -0600

    Bump 4.4.1

--- js/mailhops.js
+++ js/mailhops.js
@@ -10,7 +10,7 @@ class MailHops {
   loading = false
   tabId = null
   options = {
-    version: 'MailHops Plugin 4.4.0',
+    version: 'MailHops Plugin 4.4.1',
     api_key: '',
     owm_key: '',
     lang: 'en',
